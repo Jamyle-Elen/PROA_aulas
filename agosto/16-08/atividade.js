6a1455fa73f2f30adfe44d91c4d08c7715111215
Jamyle Elen <Jamyle Elen>
2024-08-16 16:47:59 -0300

    feat: exercises

---
+++ agosto/16-08/atividade.js
@@ -0,0 +1,33 @@
+// 1
+let a = 12;
+let b = 6;
+let c = 3;
+let mediaAritmetica = (a+b+c)/3;
+console.log(`Média aritmética = ${mediaAritmetica}`);
+
+// 2
+let n1 = 1;
+n1++;
+console.log(`Incremento: ${n1}`);
+
+// 3
+let result = 10 % 3;
+console.log(`Resto da divisão: ${result}`);
+
+// 4
+let f = 80;
+let celsius = (f - 32) / 1.8;
+
+console.log(`${f} Fahrenheit = ${celsius} Celsius`);
+
+// 5
+
+let num2 = 2;
+let metade = 2/2;
+let dobro = 2*2;
+console.log(`Dobro = ${dobro} Metade = ${metade}`);
+
+// 6
+let exp = 2**5;
+
+console.log(`2 elevado ${exp}`);
